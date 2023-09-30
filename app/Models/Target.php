fd2b7452d9f0292654164532cff9a121009c0783
= <=>
2023-09-30 20:49:58 +0100

    done implementing the Target.is_reached

--- app/Models/Target.php
+++ app/Models/Target.php
@@ -12,6 +12,8 @@ class Target extends Model
 
     protected $table = "targets";
 
+    protected $appends = ["is_reached"];
+    
     protected $fillable = ["name", "description", "account_id", "amount", "condition"];
 
     protected $casts = [
@@ -22,7 +24,13 @@ class Target extends Model
         return $this->belongsTo(Account::class);
     }
 
-    public function is_reached() {
-        // 
+    public function getIsReachedAttribute() {
+        $accounts_balance = $this->account->balance;
+        $targets_condition_operation = $this->condition->operation();
+        $targets_amount = $this->amount;
+
+        $expression = "$accounts_balance $targets_condition_operation $targets_amount";
+        $result = eval("return $expression;");
+        return $result;
     }
 }
