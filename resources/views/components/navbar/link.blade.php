d181a2ca76ceb5fc403e6d9301610e8fc153adc4
= <=>
2023-10-03 20:44:07 +0100

    fixing the bug on the background of navlink

--- resources/views/components/navbar/link.blade.php
+++ resources/views/components/navbar/link.blade.php
@@ -1,7 +1,7 @@
-@props(["href", "text", "class" => ""])
+@props(["href", "text"])
 
 @php
     $is_current_link = str_contains(url()->current(), $href);
 @endphp
 
-<a href="{{ $href }}" @class(["px-3 py-2 sm:rounded bg-transparent hover:bg-neutral-800 font-light capitalize text-sm {{ $class }}", "bg-neutral-800" => $is_current_link])>{{ $slot }}</a>
+<a href="{{ $href }}" @class(["px-3 py-2 sm:rounded hover:bg-neutral-800 font-light capitalize text-sm", "bg-neutral-800" => $is_current_link, "bg-transparent" => !$is_current_link])>{{ $slot }}</a>
