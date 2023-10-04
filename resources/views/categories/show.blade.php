49ae7090451bf48a3c988e15e6e2b6d32e9b8bd2
= <=>
2023-10-04 15:37:57 +0100

    updated the show view of all models

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -2,24 +2,18 @@
 
 @section('title', $category->name)
 
+@php
+    $back_route = route("categories.index");
+    $delete_route = route("categories.destroy", $category);
+@endphp
+
 @section('content')
 <x-container>
-    <section>
-        <section>
-            id: {{ $category->id }}
-        </section>
-        <section>
-            name: {{ $category->name }}
-        </section>
-        <section>
-            description: {{ $category->description }}
-        </section>
-        <section>
-            created_at: {{ $category->created_at }}
-        </section>
-        <section>
-            updated_at: {{ $category->updated_at }}
-        </section>
-    </section>
+    <x-vtable.table :back_route="$back_route" :delete_route="$delete_route">
+        <x-vtable.row column_name="name">{{ $category->name }}</x-vtable.row>
+        <x-vtable.row column_name="description">
+            {{ $category->description }}
+        </x-vtable.row>
+    </x-vtable.table>
 </x-container>
 @endsection
--- resources/views/incomes/show.blade.php
+++ resources/views/incomes/show.blade.php
@@ -2,27 +2,21 @@
 
 @section('title', "income")
 
+@php
+    $back_route = route("incomes.index");
+    $delete_route = route("incomes.destroy", $income);
+@endphp
+
 @section('content')
 <x-container>
-    <section>
-        <section>
-            id: {{ $income->id }}
-        </section>
-        <section>
-            date: {{ $income->transaction->date }}
-        </section>
-        <section>
-            description: {{ $income->transaction->description }}
-        </section>
-        <section>
-            amount: {{ $income->amount }}
-        </section>
-        <section>
-            category: {{ $income->category->name }}
-        </section>
-        <section>
-            account: {{ $income->account->name }}
-        </section>
-    </section>
+    <x-vtable.table :back_route="$back_route" :delete_route="$delete_route">
+        <x-vtable.row column_name="date">{{ $income->transaction->date }}</x-vtable.row>
+        <x-vtable.row column_name="description">
+            {{ $income->transaction->description }}
+        </x-vtable.row>
+        <x-vtable.row column_name="amount">{{ $income->amount }} {{ $income->account->currency->code }}</x-vtable.row>
+        <x-vtable.row column_name="account">{{ $income->account->name }}</x-vtable.row>
+        <x-vtable.row column_name="category">{{ $income->category->name }}</x-vtable.row>
+    </x-vtable.table>
 </x-container>
 @endsection
