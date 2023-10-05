b606486c66970dc3168bab51f1b2caa874d544ec
= <=>
2023-10-05 17:51:14 +0100

    minor change in the css of dropdown list

--- resources/views/partials/profile-dropdown/dropdown.blade.php
+++ resources/views/partials/profile-dropdown/dropdown.blade.php
@@ -10,7 +10,7 @@
         <img class="h-6 w-6 rounded-full" src="{{ $profile_picture_path ? asset($profile_picture_path) : "https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg" }}" alt="">
       </button>
     </section>
-    <section id="profile-dropdown-list" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+    <section id="profile-dropdown-list" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
         @include('partials.profile-dropdown.link', ["href" => route("users.profile"), "text" => "Your Profile"])
         <form action="{{ route("logout") }}" method="post" class="cursor-pointer block px-4 py-2 text-sm text-gray-700 hover:bg-primary-100">
             @csrf
